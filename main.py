9c432ddf27e30d2425b21eea31917ba05b4431a5
iksoon <ik9702@gmail.com>
2022-02-05 09:09:57 +0900

    논문 읽고 다시와라

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ d_t = 10**-4
 vol = int(rng/d_t)
 
 A = 0.1
-B = 2.
-C = 0.2
-D = 1.5
-X0 = 0
+B = 1.2
+C = 1.2
+D = -1.5
+X0 = 1
 
 T = np.zeros(vol)
 U = np.zeros(vol)
@@ -24,9 +24,9 @@ for i in range(vol):
     T[i] = i*d_t
  
 for t in range(0, vol):
-    U[t] = np.sin(T[t])
-    
-    
+    U[t] = np.sin(T[t])+0.75*np.sin(2*T[t])
+
+       
 for t in range(0, vol-1):
     X[t+1] = X[t] + (A*X[t]+B*U[t])*d_t
     Y[t+1] = C*X[t] + D*U[t]
